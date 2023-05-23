5210f216bed6d69078ea48ad9d4335e610512ddb
Gerarvvdor <00053520@uca.edu.sv>
2023-05-23 09:12:43 -0600

    Initial commit

---
+++ app/src/main/java/com/orellana/parcial_mviles_00053520/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.orellana.parcial_mviles_00053520
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
